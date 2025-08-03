f3a92e028f72c53664096ddb3f00e9c4d057b4ae
Tinku070 <bonaboinagowtham@gmail.com>
2025-08-04 00:29:35 +0530

    updated git-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/NirogGyan-Healthcare/', // ✅ Needed for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
